3d4e9e92c145fd33b41c8ff2b9394c888732dee0
sepidehchitsaz <70854585+sepidehchitsaz@users.noreply.github.com>
2021-03-25 20:03:52 -0400

    ux_ui_homework_19

--- js/index.js
+++ js/index.js
@@ -1,9 +1,36 @@
 
-$(document).ready(function(){
-    $('#content').hover(function() {
-        $("#content").addClass('transition');
+$(function(){
+    $("#frog").hover(function(){
+        $(this).width(1.25*$(this).width());
+        $(this).height(1.25*$(this).height());
+    }, function(){
+        $(this).width(0.8*$(this).width());
+        $(this).height(0.8*$(this).height());
 
-    }, function() {
-        $("#content").removeClass('transition');
     });
+
+});
+
+$(function(){
+    $("#click").hover(function(){
+        $(this).width(1.25*$(this).width());
+        $(this).height(1.25*$(this).height());
+    }, function(){
+        $(this).width(0.8*$(this).width());
+        $(this).height(0.8*$(this).height());
+
+    });
+
+});
+
+$(function(){
+    $("#education").hover(function(){
+        $(this).width(1.25*$(this).width());
+        $(this).height(1.25*$(this).height());
+    }, function(){
+        $(this).width(0.8*$(this).width());
+        $(this).height(0.8*$(this).height());
+
+    });
+
 });
